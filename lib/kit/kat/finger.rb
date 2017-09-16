7fdb3443205c7e55d72f6af07496811493470c4b
Floyd Wright <tfwright@gmail.com>
2017-09-16 18:39:26 +0000

    Renamed to better fit conventions

--- lib/kit/kat/finger.rb
+++
@@ -1,9 +0,0 @@
-require "kit/kat/finger/version"
-
-module Kit
-  module Kat
-    module Finger
-      # Your code goes here...
-    end
-  end
-end
--- lib/kit/kat/finger/version.rb
+++
@@ -1,7 +0,0 @@
-module Kit
-  module Kat
-    module Finger
-      VERSION = "0.1.0"
-    end
-  end
-end
